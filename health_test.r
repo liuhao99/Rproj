c86fa7adc35511d93f6092b940411564ccab1a5e
liye <liuhao99@yahoo.com>
2019-01-20 10:44:15 -0600

    first r

---
+++ health_test.r
@@ -0,0 +1,16 @@
+# health grade - 1: excellent, 2: good, 3: fair, 4: poor
+# gender - 1: female; 0: male
+# hospital - hospitalized in last 6 months 1: yes, 0: no
+# observation - total number of observation
+
+health <- c(1, 2, 3, 4, 1, 2, 3, 4)
+gender <- c(0, 0, 0, 0, 1, 1, 1, 1)
+hospital <- c(25, 38, 10, 4, 25, 59, 19, 3)
+observation <- c(530, 514, 81, 9, 381, 582, 93, 10)
+
+# employ.data <- data.frame(employee, salary, startdate)
+test.data <- data.frame(gender, health, hospital, observation)
+
+
+
+
